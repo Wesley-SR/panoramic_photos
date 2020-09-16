cb95800b94243790dac464db4bc3815e56f8a24d
Wesley-SR <wesley.sr@outlook.com>
2020-09-16 14:43:31 -0300

    Exception handling for communication failure with the services of each module

--- scripts/panoramic_client.py
+++ scripts/panoramic_client.py
@@ -7,19 +7,35 @@ import rospy
 from pantilt_control_ros_node_test.srv import PantiltControl
 from sony_camera_ptp.srv import SonyCameraManager
 
-def move_pantilt(send_command, command, axis):
-    print("Move pantilt")
-    response = send_command('panoramic', command, axis)
 
-    return response.response_sucess
+def move_pantilt(axis, new_angle):
+    rospy.wait_for_service('pantilt_control')
 
-def sony_take_picture(send_command, enable_liveview, enable_take_picture,
-                      key_property, value_property, file_name, path_name):
-    print("sony_take_picture\n")
-    response = send_command(
-        enable_liveview, enable_take_picture, key_property, value_property,
-        file_name, path_name)
-    return response.msg_feedback
+    try:
+        pt_send_command = rospy.ServiceProxy('pantilt_control', PantiltControl)
+        response = pt_send_command('panoramic', axis, new_angle)
+
+        return response.response_sucess
+
+    except rospy.ServiceException as e:
+        print("Service call failed: %e" % e)
+
+
+def sony_take_picture(enable_liveview, enable_take_picture, key_property,
+                      value_property, file_name, path_name):
+    rospy.wait_for_service('sony_camera_manager')
+
+    try:
+        sony_send_command = rospy.ServiceProxy(
+            'sony_camera_manager', SonyCameraManager)
+        response = sony_send_command(
+            enable_liveview, enable_take_picture, key_property, value_property,
+            file_name, path_name)
+
+        return response.msg_feedback
+
+    except rospy.ServiceException as e:
+        print("Service call failed {0}".format(e))
 
 
 def fluke_take_picture():
@@ -30,9 +46,6 @@ def fluke_take_picture():
 
 def panoramic_client():
 
-    rospy.wait_for_service('pantilt_control')
-    rospy.wait_for_service('sony_camera_manager')
-
     pan_tilt_limit = 360
     # H: 34 V: 25.5 - Sobreposicao=30%
     tilt = [0, 17.85, 35.7, 53.55, 71.4, 90]
@@ -41,54 +54,44 @@ def panoramic_client():
     returning = False
     name_photos_count = 0
 
-    try:
-        pt_send_command = rospy.ServiceProxy('pantilt_control', PantiltControl)
-        sony_send_command = rospy.ServiceProxy(
-            'sony_camera_manager', SonyCameraManager)
-        # fluke_send_command = ...
+    for idx, tilt_angle in enumerate(tilt):
 
-        for idx, tilt_angle in enumerate(tilt):
+        if tilt_angle >= pan_tilt_limit:
+            tilt_angle = pan_tilt_limit - 1
 
-            if tilt_angle >= pan_tilt_limit:
-                tilt_angle = pan_tilt_limit - 1
+        pantilt_feedback = move_pantilt('tilt', tilt_angle)
+        if pantilt_feedback:
 
-            pantilt_feedback = move_pantilt(pt_send_command, 'tilt', tilt_angle)
-            if pantilt_feedback:
+            current_step = pan_steps[idx]
+            steps_count = math.ceil(360 / current_step)
+            pan_photos_count = 0
 
-                current_step = pan_steps[idx]
-                steps_count = math.ceil(360 / current_step)
-                pan_photos_count = 0
+            while pan_photos_count < steps_count:
+                if returning:
+                    current_angle = 360 - pan_photos_count * current_step
 
-                while pan_photos_count < steps_count:
-                    if returning:
-                        current_angle = 360 - pan_photos_count * current_step
+                else:
+                    current_angle = pan_photos_count * current_step
 
-                    else:
-                        current_angle = pan_photos_count * current_step
+                if current_angle >= pan_tilt_limit:
+                    current_angle = pan_tilt_limit - 1
 
-                    if current_angle >= pan_tilt_limit:
-                        current_angle = pan_tilt_limit - 1
+                pantilt_feedback = move_pantilt('pan', current_angle)
 
-                    pantilt_feedback = move_pantilt(
-                        pt_send_command, 'pan', current_angle)
+                if pantilt_feedback:
+                    sony_feedback = sony_take_picture(
+                        , False, True, "", "", str(name_photos_count), "")
+ 
+                    fluke_feedback = fluke_take_picture()
 
-                    if pantilt_feedback:
-                        sony_feedback = sony_take_picture(
-                            sony_send_command, False, True,
-                            "", "", str(name_photos_count), "")
-                        
-                        fluke_feedback = fluke_take_picture()
+                    if sony_feedback == "Values are setted" and
+                                        fluke_feedback == True:
+                        pan_photos_count = pan_photos_count + 1
+                        name_photos_count = name_photos_count + 1
 
-                        if sony_feedback == "Values are setted" and
-                                             fluke_feedback == True:
-                            pan_photos_count = pan_photos_count + 1
-                            name_photos_count = name_photos_count + 1
-
-            returning = not returning  # Reverse direction of rotation
-
-    except rospy.ServiceException as e:
-        print("Service call failed {0}".format(e))
+        returning = not returning  # Reverse direction of rotation
 
 
 if __name__ == "__main__":
-    panoramic_client()
+    
+    panoramic.panoramic_client()
